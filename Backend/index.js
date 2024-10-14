7202430b642a1e7d3bb771eafeac2a3e9a10c5d1
khushvant kumar <98275946+khushvantkr12@users.noreply.github.com>
2024-10-14 14:50:27 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOptions= {
-    origin:'https://job-portal-blush-rho.vercel.app',
+    origin:'https://job-portal-blush-rho.vercel.app/',
     credentials:true
 }
 app.use(cors(corsOptions));
